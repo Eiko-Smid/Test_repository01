ead9f15bc7905f4c055151fb18f3280339af96d0
Eiko-Smid <smid.eiko@gmail.com>
2020-06-24 09:30:34 +0200

    test

---
+++ hallo_pi.py
@@ -0,0 +1,22 @@
+# from gpiozero import LED
+# from time import sleep 
+
+
+# red = LED(17)
+
+# while True:
+#     red.on()
+#     sleep(1)
+#     red.off()
+#     sleep(1)
+
+def addAndPrint(a, b):
+    c = a+b
+    print("Das Ergebnis lautet: ", c)
+    return c
+
+
+
+addAndPrint(10, 5)
+
+
